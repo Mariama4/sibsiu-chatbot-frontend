7ad0837b32f542b91b5652438cc6108c050d5714
mariama4 <mariama4@mail.ru>
2022-10-04 00:44:35 +0700

    upd

--- src/components/FrameList.jsx
+++ src/components/FrameList.jsx
@@ -8,44 +8,12 @@ import FrameItem from "./FrameItem";
 
 const FrameList = observer((props) => {
   const addNewFrame = async () => {
-    const formData = new FormData();
-    formData.append(
-      "data",
-      JSON.stringify({
-        ID: "EXAMPLE ID - id для связи с другими frame`ми",
-        DATA: {
-          MESSAGE: {
-            TYPE: "TEXT",
-            PARSE_MODE: "HTML",
-            TEXT: "start!",
-            PHOTO: "photo url...",
-            PHOTO_CAPTION: "photo caption",
-            MEDIA_GROUP: ["url 1", "url 2", "url 3"],
-            MEDIA_CAPTION: "media caption",
-            VIDEO_NOTE: "video note url",
-            VIDEO_NOTE_CAPTION: "note caption",
-            VENUE_LATITUDE: "venue latitude",
-            VENUE_LONGITUDE: "venue longitude",
-            VENUE_TITLE: "venue title",
-            VENUE_ADDRESS: "venue address",
-            VENUE_CAPTION: "venue caption..",
-            CONTACT_PHONE_NUMBER: "contact phone number..",
-            CONTACT_FIRST_NAME: "contact first name",
-            CONTACT_LAST_NAME: "contact last name",
-            CONTACT_CAPTION: "contact caption..",
-          },
-          BUTTONS: [
-            {
-              TEXT: "text btn 1",
-              REDIRECT_ON_ID_FRAME: "frame 1",
-            },
-          ],
-        },
-      })
-    );
-    await addFrame(formData);
-    const response = await getFrames();
-    props.list.setFrames(response["frames"]);
+    (async () => {
+      return await addFrame();
+    }).then(async (e) => {
+      const response = await getFrames();
+      props.list.setFrames(response["frames"]);
+    });
   };
 
   const delFrame = async (id) => {
