3c063c3aaffcbeb5fa44e9935ede5afab6fabc12
mariama4 <mariama4@mail.ru>
2022-10-17 23:57:07 +0700

    upd

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,22 @@
 import { Container } from "react-bootstrap";
-import Spinner from "react-bootstrap/Spinner";
+import { Dna } from "react-loader-spinner";
 
 const Loading = () => {
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "80vh" }}
+      fluid
+    >
+      <Dna
+        visible={true}
+        height="180"
+        width="180"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      />
+    </Container>
   );
 };
 
